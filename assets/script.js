b4eae6c1adf5007041a6c937b324064e5c92c656
Mohammad Zafar <sz7861@outlook.com>
2021-08-15 18:09:35 +0100

    Convert to 12 hour format

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,7 @@
 // Show date format
 var loadTodayDate = moment().format("dddd, MMMM Do YYYY");
 var usePresentHour = parseInt(moment().format("H")); 
+// Convert to 12 hour format
+function twelveHourFormat(hourOfDay) {
+    return parseInt(moment(hourOfDay,["h A"]).format("HH"))
+}
