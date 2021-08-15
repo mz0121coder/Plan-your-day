ca31ccecb232ee78384ba68d9e8a1974fad05f8e
Mohammad Zafar <sz7861@outlook.com>
2021-08-15 18:09:52 +0100

    Save activities in local storage

--- assets/script.js
+++ assets/script.js
@@ -5,3 +5,11 @@ var usePresentHour = parseInt(moment().format("H"));
 function twelveHourFormat(hourOfDay) {
     return parseInt(moment(hourOfDay,["h A"]).format("HH"))
 }
+// Save activities in local storage
+function saveActivity(event){
+    var textarea = $(event.currentTarget.parentElement).find("textarea")[0]; 
+    var hour = $(event.currentTarget.parentElement).find("div")[0].textContent;
+    var timeDisplay = twelveHourFormat(hour);
+    var activityPlan = $(textarea).val();
+    localStorage.setItem("item-" + loadTodayDate + '-' + timeDisplay, activityPlan) 
+}
