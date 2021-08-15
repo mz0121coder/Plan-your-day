73b3472a82b6167bda2b58315ec017f94dd871cd
Mohammad Zafar <sz7861@outlook.com>
2021-08-15 18:11:52 +0100

    Retrieve items from local storage

--- assets/script.js
+++ assets/script.js
@@ -9,7 +9,30 @@ function twelveHourFormat(hourOfDay) {
 function saveActivity(event){
     var textarea = $(event.currentTarget.parentElement).find("textarea")[0]; 
     var hour = $(event.currentTarget.parentElement).find("div")[0].textContent;
-    var timeDisplay = twelveHourFormat(hour);
+    var displayTimeActivity = twelveHourFormat(hour);
     var activityPlan = $(textarea).val();
-    localStorage.setItem("item-" + loadTodayDate + '-' + timeDisplay, activityPlan) 
+    localStorage.setItem("item-" + loadTodayDate + '-' + displayTimeActivity, activityPlan) 
+}
+// Retrieve items from local storage
+function showHourSections() {
+    var $planInputSpace = $("textarea");
+
+    $planInputSpace.each(function(key, activityPlan) {
+        var hour = $(activityPlan.parentElement).find("div")[0].textContent; 
+        var displayTimeActivity = twelveHourFormat(hour);
+        var text = localStorage.getItem("item-" + loadTodayDate + '-' + displayTimeActivity) || ""; 
+        $(activityPlan).val(text)
+    
+        if(displayTimeActivity < usePresentHour){
+            $(activityPlan).addClass("past");     
+        }
+
+        if(displayTimeActivity > usePresentHour){
+            $(activityPlan).addClass("future");
+        }
+    
+        if(displayTimeActivity == usePresentHour){
+            $(activityPlan).addClass("present");
+        } 
+    });
 }
